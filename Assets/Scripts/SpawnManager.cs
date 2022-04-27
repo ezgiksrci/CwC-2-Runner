40efef85f2804758f3808e60acc7e86bef16bb97
Ezgi Keserci <keserci.ezgi@gmail.com>
2022-04-27 22:27:33 +0300

    Added Spawn manager, obstacle, repeat background script, move left script.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacles", startDelay, repeatRate);
+    }
+
+    void SpawnObstacles()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+
+}
